455e8a5a2d60992b471d36ea29e458dba1d6c3fc
Ujjawal Maurya <chocolateboyz0011@gmail.com>
2023-01-07 21:26:52 +0530

    first commit

---
+++ 01-JShomework/hw.js
@@ -0,0 +1,153 @@
+//TODO: #1 - Variables
+
+// //* Initialisation
+// var str = "STRING"
+// var num = 157
+// var boolValues = true
+// var val1 = undefined
+// var val2 = null
+
+// //* Printing them
+// console.log(str);
+// console.log(num);
+// console.log(boolValues);
+// console.log(val1);
+// console.log(val2);
+
+//TODO: #2 - Interpoletion method
+
+// var fName = "Ujjawal";
+// var lName = "Maurya";
+// var marital_status = 'Un-Married';
+// var country = "India";
+// var age = 50;
+
+// console.log(`User ${fName} ${lName} of age ${age} from ${country} and is ${marital_status}.`);
+
+// TODO: #3 - toUpperCase()
+
+// var string = "hello world.!!!";
+// console.log(string.toUpperCase());
+
+// TODO: #4 - include()
+
+// var value = "A Cleaver bworn fox quick jumps."
+// console.log(value.includes("y"));
+
+// TODO: #5 - split()
+// let str = "Ujjawal Maurya";
+// let arr = [];
+// arr = str.split('');
+// console.log(arr);
+
+// TODO: #6 - split()
+// let str = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+// let arr = str.split(",")
+// console.log(arr);
+
+// TODO: #7 - lastIndexOf()  ====================== Pending
+// let str = "Hello hello hello world, I open my eyes and say hello to the world !";
+// console.log(str.lastIndexOf("I"));
+
+
+// TODO: #8 -  Search
+// var str = "You cannot end a sentence with because, because! because is a conjunction.";
+
+// var res = str.indexOf("because")
+
+// console.log(res); // Ans - 31
+
+
+// TODO: #9 - trim()
+// var str = "   Ujjawal "
+
+// console.log(str.trim(""));
+
+// TODO: #10 - Boolean values - True/False  ============= Pending
+
+
+// TODO: #11 - Judge outputs by youself
+// - 4 > 3 - true
+// - 4 >= 3 - true
+// - 4 < 3 - false
+// - 4 <= 3 - false
+// - 4 == 4 - true
+// - 4 === 4 - true
+// - 4 != 4 - false
+// - 4 !== 4 - false
+// - 4 != '4' - false
+// - 4 == '4' - true
+// - 4 === '4' - false
+
+// - Find the length of python and jargon and make a falsy comparison statement.
+// var str1 = 'python'
+// var str2 = 'jargon'
+
+// console.log(str2.length != str1.length); // false
+
+// TODO: #12 - operations with Date()
+// const date = new Date()
+//  - Year? - console.log(date.getFullYear());
+//  - month in number? - console.log(date.getMonth() + 1);
+//  - today's date? - console.log(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`);
+//  - today's day in number? - console.log(date.getDay());
+//  - current hour? - console.log(date.getHours());
+//  - current minute? - console.log(date.getMinutes());
+//  - number of seconds elapsed from January 1, 1970 to now?
+//   var oldDate = new Date(1970, 1, 1)
+//   console.log(date - oldDate);
+
+// TODO: #13 - Find area of a triangle.
+// var base = prompt("Enter Base:")
+// var height = prompt("Enter Height:")
+
+// console.log(` Area of that triangle: ${0.5 * base * height}`);
+
+// TODO: #14 - find m = ?
+// var x1 = 2
+// var x2 = 6
+// var y1 = 2
+// var y2 = 10
+// m = (y2-y1)/(x2-x1)
+
+// console.log(m);
+
+// TODO: #15 - calculate slope
+
+// TODO: #16 - Get radius
+const r = prompt("Radius: ")
+
+// TODO: #17 - 
+// TODO: #18 - 
+// TODO: #19 - 
+// TODO: #20 - 
+// TODO: #21 - 
+// TODO: #22 - 
+// TODO: #23 - 
+// TODO: #24 - 
+// TODO: #25 - 
+// TODO: #26 - 
+// TODO: #27 - 
+// TODO: #28 - 
+// TODO: #29 - 
+// TODO: #30 - 
+// TODO: #31 - 
+// TODO: #32 - 
+// TODO: #33 - 
+// TODO: #34 - 
+// TODO: #35 - 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
